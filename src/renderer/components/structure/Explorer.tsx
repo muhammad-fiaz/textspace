9318ed5e322fbeb6da5c0270ba415c1c0649c921
Muhammad Fiaz <s.muhammadfiaz2003@gmail.com>
2023-07-23 17:15:38 +0530

    🎶 Added Title bar menubar

---
+++ src/renderer/components/structure/Explorer.tsx
@@ -0,0 +1,15 @@
+import '../../../styles/explorer.scss';
+import React from 'react';
+
+function Explorer() {
+  return (
+    <div className="explorer">
+
+
+
+    </div>
+
+  );
+}
+
+export default Explorer;
